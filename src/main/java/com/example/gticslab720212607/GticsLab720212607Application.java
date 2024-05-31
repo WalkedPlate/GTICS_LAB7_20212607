aef5da8c4b8e1675a4b5618d369d0a120622236d
Arkel <144744565+WalkedPlate@users.noreply.github.com>
2024-05-30 19:13:46 -0500

    first commit

---
+++ src/main/java/com/example/gticslab720212607/GticsLab720212607Application.java
@@ -0,0 +1,13 @@
+package com.example.gticslab720212607;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab720212607Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GticsLab720212607Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gticslab720212607/GticsLab720212607ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gticslab720212607;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab720212607ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
